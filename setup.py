9f505918792af5fe7569bc6336a554e580922bbe
Andrew Rabert <ar@nullsum.net>
2018-07-28 19:21:15 -0400

    Update metadata

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 
 setuptools.setup(
     name='subsonic-cli',
-    version='0.0.1',
+    version='0.1.0',
     author='Andrew Rabert',
-    author_email='arabert@nullsum.net',
+    author_email='ar@nullsum.net',
     license='Apache 2.0',
     py_modules=['subsonic_cli'],
     install_requires=['requests'],
